c7f5e7b75b3978332e8c6794a317bb8c84f03295
Siddharth <kartikx04@gmail.com>
2024-11-19 16:13:49 +0530

    Connection initialized using mux router which is handling getUsers and createUsers

---
+++ api.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type api struct {
+	addr string
+}
+
+var users = []User{}
+
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var payload User
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	u := User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+	}
+
+	if err := insertUser(u); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func insertUser(u User) error {
+	//input validation
+	if u.FirstName == "" {
+		return errors.New("first name is required")
+	}
+	if u.LastName == "" {
+		return errors.New("first name is required")
+	}
+
+	//storage validation
+	for _, user := range users {
+		if user.FirstName == u.FirstName && user.LastName == u.LastName {
+			return errors.New("user already exist")
+		}
+	}
+
+	users = append(users, u)
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module httpServerPackage
+
+go 1.22.5
